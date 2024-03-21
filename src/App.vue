5ff04266e6a1f19e38e55553f67a264d886f4850
Himanshu <himanshu8747@gmail.com>
2024-03-21 11:16:13 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Parent/>
+    <UserData/>
+    <FormData/>
+  </div>
+</template>
+
+<script>
+import Parent from './components/ParentChildQ1/Parent.vue';
+import UserData from './components/UsersList/UserData.vue';
+import FormData from './components/Form/FormData.vue';
+export default {
+  name: 'App',
+  components: {
+    Parent,
+    UserData,
+    FormData,
+  },
+};
+</script>
